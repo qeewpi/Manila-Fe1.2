998bea5bb84c6173913394a2aa43decc355ab02e
smurfmigos <smurfmigos@gmail.com>
2024-03-21 10:43:57 +0800

    Member #3

--- src/components/pages/map/Map.js
+++ src/components/pages/map/Map.js
@@ -7,10 +7,9 @@ import employees from './data.json';
 
 function Map() {
   // using the useState hook to create a state variable for user chosen color
-  const [color, setColor] = useState("white");
 
   return (
-    <div className="content" style={{ backgroundColor: color }}>
+    <div>
 
       <p>Miguel T. Soniel SN: 2021104644</p>
 
